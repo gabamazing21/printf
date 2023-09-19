0db0461ea884d6c96d0ce12790f6f3125ae0726a
gabriel <gabeamazing21@gmail.com>
2023-09-19 19:55:28 +0000

    printf

--- 0-printf.c
+++ 0-printf.c
@@ -8,10 +8,27 @@ int _printf(const char *format, ...)
 {
 	va_list ptr;
 	const char formater;
-	int i;
+	int i,va;
 
 	va_start(ptr, format);
 	if (format == NULL)
 		return (NULL);
-	for (i = 0, 
+	for (i = 0; *format != '\0'; i++)
+	{
+		if ( *format == 'c')
+		{
+			val = va_arg(ptr, char);
+			_putchar(val);
+
+		}
+		else if ( *format == 's')
+		{
+			val = va_arg(ptr, char);
+		}
+		else if (*format == '%')
+		{
+			val = va_arg(ptr, char);
+		}
+	}
+	_putchar('\n');
 }
