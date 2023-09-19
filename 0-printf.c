80541553b06bcf75cae8239414a74c239b59c07d
NunuCodes11 <lawalhamzat11@gmail.com>
2023-09-19 16:03:27 +0100

    done w task 0

--- 0-printf.c
+++ 0-printf.c
@@ -1,17 +1,67 @@
 #include "main.h"
+#include <unistd.h>
+int _printf(const string format, ...);
 /**
- * _printf - print to standard output according to a format specify
- * format - format can be c, s, %
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _printf(const char *format, ...)
+int _putchar(char c)
 {
-	va_list ptr;
-	const char formater;
-	int i;
+	return (write(1, &c, 1));
+}
 
-	va_start(ptr, format);
+/**
+ * _printf - function that produces output according to a format.
+ * @format: a character string
+ * Return: length of the output
+ */
+int _printf(const string format, ...)
+{
+	specifier_format d[] =  {
+		{"c", _print_char}, {"s", _print_str},
+		{"%", _print_mod}
+	};
+	va_list varArg;
+	int len = 0, i = 0, j;
+
+	va_start(varArg, format);
+
 	if (format == NULL)
-		return (NULL);
-	for (i = 0, 
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		else
+		{
+			if (format[i + 1] == '\0' &&  format[i] == '%')
+				return (-1);
+			j = 0;
+			while (j < 3)
+			{
+				if (format[i + 1] == *(d[j].spec))
+					break;
+				j++;
+			}
+			if (j < 3)
+			{
+				len += d[j].func(varArg);
+				i += 1;
+			}
+			else
+			{
+				_putchar(format[i]);
+				len++;
+			}
+		}
+		i++;
+	}
+	va_end(varArg);
+	return (len);
 }
